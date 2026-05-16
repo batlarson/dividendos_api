995aa2c7cb9bbacde6ee16382b454ab67ec6345c
Ibon Mugica <iboedu@hotmail.com>
2026-05-16 06:01:59 +0200

    Inicio proyecto: modelos y serializers de portfolio

---
+++ portfolio/models.py
@@ -0,0 +1,34 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Activo(models.Model):
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE)
+    nombre = models.CharField(max_length=50)
+    ticker = models.CharField(max_length=5)
+    precio = models.DecimalField(max_digits=10, decimal_places=2)
+    cantidad = models.IntegerField()
+
+    def __str__(self):
+        return self.nombre
+
+class Compra(models.Model):
+    activo = models.ForeignKey(Activo, on_delete=models.CASCADE) 
+    fecha_compra = models.DateField()
+    precio = models.DecimalField(max_digits=10, decimal_places=2)
+    cantidad = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.activo} - {self.fecha_compra}"
+
+class Dividendo(models.Model):
+    activo = models.ForeignKey(Activo, on_delete=models.CASCADE) 
+    fecha_pago = models.DateField()
+    div_origen = models.DecimalField(max_digits=20, decimal_places=2)
+    cambio_nominal = models.DecimalField(max_digits=10, decimal_places=2)
+    impuesto = models.DecimalField(max_digits=5, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.activo} | {self.fecha_pago} | {self.importe_neto()}€"
+    
+    def importe_neto(self):
+        return self.div_origen * self.cambio_nominal * (1 - self.impuesto / 100)
---
+++ portfolio/serializers.py
@@ -0,0 +1,20 @@
+from rest_framework import serializers
+from .models import Activo, Compra, Dividendo
+
+class ActivoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Activo
+        fields = ['id', 'usuario', 'precio', 'ticker', 'nombre', 'cantidad']
+        read_only_fields = ['id', 'usuario']
+
+class CompraSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Compra
+        fields = ['id', 'activo', 'fecha_compra', 'precio', 'cantidad']
+        read_only_fields = ['id']
+
+class DividendoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Dividendo
+        fields = ['id', 'activo', 'fecha_pago','cambio_nominal', 'impuesto']
+        read_only_fields = ['id']
