d809c0fb7ed4bba225a0c2fbff6ab775c9ad6047
Ibon Mugica <iboedu@hotmail.com>
2026-05-20 03:58:45 +0200

    Fix: La view se ve perfecta y coge bien los datos

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from rest_framework import viewsets
 from rest_framework.response import Response
+from rest_framework.decorators import action
+from django.utils import timezone
+from datetime import timedelta
 from rest_framework.permissions import IsAuthenticated
+from django.db.models import Count, Sum, F, Avg
 from .models import Activo, Compra, Dividendo
 from .serializers import ActivoSerializer, DividendoSerializer, CompraSerializer
 
@@ -14,6 +18,32 @@ class ActivoViewSet(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         serializer.save(usuario=self.request.user)
+
+    @action(detail=False, methods=['get'])
+    def resumen(self, request):
+        hace_un_año = timezone.now().date() - timedelta(days=365)
+        total_activos = self.get_queryset().count()
+        total_invertido = (Compra.objects.filter(
+            activo__usuario = request.user,
+        ).aggregate(
+            total_precio=Sum(F('precio')* F('cantidad'))
+        ))
+        dividendos_anuales = (Dividendo.objects.filter(
+            activo__usuario = request.user,
+            fecha_pago__gte = hace_un_año
+        ).aggregate(
+            total_div=Sum('div_origen')
+        ))
+        if dividendos_anuales['total_div'] and total_invertido['total_precio']:
+            yoc_global = dividendos_anuales['total_div'] / total_invertido['total_precio'] * 100
+        else:
+            yoc_global = None
+        return Response({
+            'total_activos': total_activos,
+            'total_invertido': total_invertido['total_precio'],
+            'dividendos_anuales': dividendos_anuales['total_div'],
+            'yoc_global': yoc_global
+        })
 
 class CompraViewSet(viewsets.ModelViewSet):
     serializer_class = CompraSerializer
