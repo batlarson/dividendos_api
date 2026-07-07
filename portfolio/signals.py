54eef0696d5687c60d5b9e69a157a29721910c15
Ibon Mugica <iboedu@hotmail.com>
2026-07-07 06:31:34 +0200

    Convertir la funcion de crear historial a asíncrona con Celery

--- portfolio/signals.py
+++ portfolio/signals.py
@@ -1,11 +1,9 @@
 from django.dispatch import receiver
 from django.db.models.signals import post_save
 from .models import Historial, Dividendo
+from .tasks import procesar_dividendo
 
 @receiver(post_save, sender=Dividendo)
 def dividendo_agregado(sender, instance, created, **kwargs):
     if created:
-        Historial.objects.create(
-            activo = instance.activo,
-            mensaje=f"{instance.activo.ticker} te ha pagado {instance.importe_neto()} el {instance.fecha_pago}"
-        )
+        procesar_dividendo.delay(instance.id)  # manda el ID a Celery
--- portfolio/tasks.py
+++ portfolio/tasks.py
@@ -1,5 +1,14 @@
 from celery import shared_task
+from .models import Dividendo, Historial
 
 @shared_task
 def tarea_prueba():
     return "Celery funciona!"
+
+@shared_task
+def procesar_dividendo(dividendo_id):
+    dividendo = Dividendo.objects.get(id=dividendo_id)
+    Historial.objects.create(
+        activo=dividendo.activo,
+        mensaje=f"{dividendo.activo.ticker} te ha pagado {dividendo.importe_neto()} el {dividendo.fecha_pago}"
+    )
