72e90d66b885b3e40045a69d5a96ef3a55277971
Ibon Mugica <iboedu@hotmail.com>
2026-07-19 06:45:59 +0200

    Permiso para actualizar solo entre semana

--- portfolio/permissions.py
+++ portfolio/permissions.py
@@ -1,5 +1,14 @@
 from rest_framework.permissions import BasePermission
+import datetime
 
 class EsDuenoDelActivo(BasePermission):
     def has_object_permission(self, request, view, obj):
         return obj.usuario == request.user
+    
+class ActualizarEntreSemana(BasePermission):
+    def has_permission(self, request, view):
+        if request.method in ['PUT', 'PATCH']:
+            dia = datetime.date.today().weekday()
+            if dia >= 5:
+                return False
+        return True
